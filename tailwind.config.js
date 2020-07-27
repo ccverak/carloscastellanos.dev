1fe2c3eda487339476c46df1657a9eeed489b848
Carlos Castellanos <ccverak@gmail.com>
2020-07-27 12:59:07 +0200

    Fixes in the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,34 +4,31 @@ const config = {
   theme: {
     extend: {
       fontFamily: {
-        serif: ['Merriweather', ...defaultTheme.fontFamily.serif],
-        sans: ['Inter', 'IBM Plex Sans', ...defaultTheme.fontFamily.sans]
-      }
+        serif: ["Merriweather", ...defaultTheme.fontFamily.serif],
+        sans: ["Merriweather", defaultTheme.fontFamily.sans],
+      },
     },
     boxShadow: {
-      focus: 'inset 0 -.43333em #54c8f5a3'
+      focus: "inset 0 -.43333em #54c8f5a3",
     },
     backgroundColor: {
       ...defaultTheme.colors,
-      link: '#9ddfffa6'
+      link: "#9ddfffa6",
     },
     minHeight: {
-      '0': '0',
-      '1/4': '25%',
-      '1/2': '50%',
-      '3/4': '75%',
-      '9/10': '90%',
-      full: '100%'
-    }
+      "0": "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      "9/10": "90%",
+      full: "100%",
+    },
   },
   variants: {},
   plugins: [
     //require('@tailwindcss/typography')
   ],
-  purge: [
-    './src/**/*.html',
-    './src/**/*.njk'
-  ],
-}
+  purge: ["./src/**/*.html", "./src/**/*.njk"],
+};
 
 module.exports = config
