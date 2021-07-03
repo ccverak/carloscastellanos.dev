668d3050e5c6d2637689136df789149d5374e2fb
Carlos Castellanos (unused account) <ccverak@gmail.com>
2021-07-03 13:34:59 +0200

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,21 +4,21 @@
 
 module.exports = {
   lang: 'en', // for html tag
-  title: 'Carlos Castellanos - @ccverak',
-  description: 'Carlos Castellanos Vera is a Software Engineer and Solutions Architect located in Malaga, Spain with over 10 years of experience.',
+  title: 'Carlos Castellanos - @karloscodes',
+  description: 'Carlos Castellanos is a Software Engineer and Solutions Architect located in Malaga, Spain with over 10 years of experience.',
   url: 'https://carloscastellanos.dev', // don't end with a slash /
 
   author: {
     name: 'Carlos Castellanos', // for posts meta and Open Graph meta (FB and Twitter)
     email: 'me@carloscastellanos.dev', // used in legal page
-    github: 'https://github.com/ccverak', // used in footer
-    twitter: 'https://twitter.com/ccverak' // used in footer
+    github: 'https://github.com/karloscodes', // used in footer
+    twitter: 'https://twitter.com/karloscodes' // used in footer
   },
 
   metadata: {
     theme_color: '#1e2327', // used in Chrome, Firefox OS and Opera
     default_social_image: 'https://carloscastellanos.dev/assets/images/avatar.jpg', // for Open Graph meta
     locale: 'en_US', // for Open Graph meta
-    twitter_username: '@ccverak' // for Twitter Open Graph meta
+    twitter_username: '@karloscodes' // for Twitter Open Graph meta
   }
 }
